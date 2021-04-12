ccf8efb22ebb77531631f8fe8d458ef35113a768
David Nisbet <nizz3hx@protonmail.com>
2021-04-12 23:05:09 +0100

    Worked more on this

--- app.js
+++ app.js
@@ -3,11 +3,16 @@ const express = require('express')
 const dotenv = require('dotenv')
 const morgan = require('morgan')
 const exphbs = require('express-handlebars')
+const passport = require('passport')
+const session = require('express-session')
 const connectDB = require('./config/db')
 
 // Load config
 
 dotenv.config({ path: './config/config.env' })
+
+// Passport Config
+require('./config/passport')(passport)
 
 connectDB()
 
@@ -21,11 +26,23 @@ if(process.env.NODE_ENV === 'dev'){
 app.engine('.hbs', exphbs({defaultLayout: 'main', extname: '.hbs'}))
 app.set('view engine', '.hbs')
 
+//Sessions Middleware
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: false,
+}))
+
+// Passport Middleware
+app.use(passport.initialize())
+app.use(passport.session())
+
 // Static Folder
 app.use(express.static(path.join(__dirname, 'public')))
 
 //routes
 app.use('/', require('./routes/index'))
+app.use('/auth', require('./routes/auth'))
 
 const PORT = process.env.PORT || 3000
 
