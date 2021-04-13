c59e5b17cad558c33e93191e68bcc35341176b1d
David Nisbet <nizz3hx@protonmail.com>
2021-04-13 17:21:23 +0100

    Can now delete, edit, and see a user stories

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose')
 const dotenv = require('dotenv')
 const morgan = require('morgan')
 const exphbs = require('express-handlebars')
+const methodOverride = require('method-override')
 const passport = require('passport')
 const session = require('express-session')
 const MongoStore = require('connect-mongo')(session)
@@ -28,14 +29,28 @@ if(process.env.NODE_ENV === 'dev'){
     app.use(morgan('dev'))
 }
 
+//Method Override
+app.use(
+    methodOverride(function (req, res) {
+      if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+        // look in urlencoded POST bodies and delete it
+        let method = req.body._method
+        delete req.body._method
+        return method
+      }
+    })
+  )
+
 //Handlebars Helpers
-const { formatDate, stripTags, truncate } = require('./helpers/hbs')
+const { formatDate, stripTags, truncate, editIcon, select } = require('./helpers/hbs')
 
 // Handlebars
 app.engine('.hbs', exphbs({ helpers:{
     formatDate,
     stripTags,
     truncate,
+    editIcon,
+    select,
 }, defaultLayout: 'main', extname: '.hbs'}))
 app.set('view engine', '.hbs')
 
@@ -51,6 +66,12 @@ app.use(
 // Passport Middleware
 app.use(passport.initialize())
 app.use(passport.session())
+
+//Set global var
+app.use(function(req, res, next) {
+    res.locals.user = req.user || null
+    next()
+})
 
 // Static Folder
 app.use(express.static(path.join(__dirname, 'public')))
